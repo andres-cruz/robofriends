3f1e26ef98fe19c8526e292c3cf4a457bc837ff3
Andres <andrescruz.tech@gmail.com>
2019-03-13 14:17:36 -0300

    Update code styling

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,25 +4,7 @@ import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
-
 import { setSearchField, requestRobots } from '../actions';
-
-const mapStateToProps = (state) => {
-    return {
-        searchField: state.searchRobots.searchField,
-        robots: state.requestRobots.robots,
-        isPending: state.requestRobots.isPending,
-        error: state.requestRobots.error
-    };
-}
-
-const mapDispatchToProps = (dispatch) => {
-    return {
-        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
-        onRequestRobots: () => dispatch(requestRobots())
-    };
-}
-
 
 class App extends Component {
     componentDidMount() {
@@ -45,6 +27,22 @@ class App extends Component {
                 </div>
             );
     }
+}
+
+const mapStateToProps = (state) => {
+    return {
+        searchField: state.searchRobots.searchField,
+        robots: state.requestRobots.robots,
+        isPending: state.requestRobots.isPending,
+        error: state.requestRobots.error
+    };
+}
+
+const mapDispatchToProps = (dispatch) => {
+    return {
+        onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+        onRequestRobots: () => dispatch(requestRobots())
+    };
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
--- src/reducers.js
+++ src/reducers.js
@@ -12,7 +12,7 @@ const initialStateSearch = {
 export const searchRobots = (state=initialStateSearch, action={}) => {
     switch(action.type) {
         case CHANGE_SEARCH_FIELD:
-            return Object.assign({}, state,{ searchField: action.payload });
+            return { ...state, searchField: action.payload };
         default:
             return state;            
     }
@@ -27,11 +27,11 @@ const initialStateRobots = {
 export const requestRobots = (state = initialStateRobots, action={}) => {
     switch(action.type) {
         case REQUEST_ROBOTS_PENDING:
-            return Object.assign({}, state, { isPending: true });
+            return { ...state, isPending: true };
         case REQUEST_ROBOTS_SUCCESS:
-            return Object.assign({}, state, { robots: action.payload, isPending: false });    
+            return { ...state, robots: action.payload, isPending: false };    
         case REQUEST_ROBOTS_FAILED:
-            return Object.assign({}, state, { error: action.payload, isPending: false });
+            return { ...state, error: action.payload, isPending: false };
     default:
         return state;            
     }
